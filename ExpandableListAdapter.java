07abe1bf3bdc7235f7f14c0ec1d5dd9bf8f1ef0a
oukelimokafei-oss <oukelimokafei@gmail.com>
2026-04-28 13:24:08 +0800

    Create ExpandableListAdapter.java

---
+++ ExpandableListAdapter.java
@@ -0,0 +1,87 @@
+package com.example.campusexpress;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import java.util.HashMap;
+import java.util.List;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> groupList;
+    private HashMap<String, List<String>> childMap;
+
+    public ExpandableListAdapter(Context context, List<String> groupList, HashMap<String, List<String>> childMap) {
+        this.context = context;
+        this.groupList = groupList;
+        this.childMap = childMap;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return groupList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        String groupName = groupList.get(groupPosition);
+        return childMap.get(groupName).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return groupList.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        String groupName = groupList.get(groupPosition);
+        return childMap.get(groupName).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(android.R.layout.simple_expandable_list_item_1, null);
+        }
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText(groupList.get(groupPosition));
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
+        }
+        TextView textView = convertView.findViewById(android.R.id.text1);
+        textView.setText((String) getChild(groupPosition, childPosition));
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
